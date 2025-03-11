f84c3c6f23ceb3acb0986083df2705c51bebc800
shubham431997 <142888485+shubham431997@users.noreply.github.com>
2025-03-11 13:05:45 +0530

    Update upload.js

--- src/_middleware/upload.js
+++ src/_middleware/upload.js
@@ -1,13 +1,9 @@
 import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // Define the uploads directory inside the `src` folder
-const uploadsDir = path.join(__dirname, 'uploads');
+const uploadsDir = path.join(process.cwd(), 'src', 'uploads');
 
 // Ensure the uploads directory exists
 if (!fs.existsSync(uploadsDir)) {
